f095d43c387415ba8d378ecb0ef7b553d1387a17
topanb11 <topanbudiman11@gmail.com>
2022-12-04 13:49:35 -0700

    Fixed ru table

--- backend/src/main/resources/db/migration/V5__BulkInsert.sql
+++ backend/src/main/resources/db/migration/V5__BulkInsert.sql
@@ -20,7 +20,7 @@ VALUES
 INSERT INTO ru (id, firstname, lastname, email, password, address, creditcardnumber, creditcardexpirationdate, ccv, validUntil)
 VALUES
 	(uuid_generate_v4(), 'Topan', 'Nguyen', 'topan@gmail.com', 'test', '123 street', '3423458255', '01/08/29', '555', '2023-12-04T17:22:28.814+00:00'),
-	(uuid_generate_v4(), 'Topan', 'Nguyen', 'topan@gmail.com', 'test', '123 street', '3423458255', '01/08/29', '555', '2022-12-03T17:22:28.814+00:00');
+	(uuid_generate_v4(), 'Brian', 'Nguyen', 'brian@gmail.com', 'test', '123 street', '3423458255', '01/08/29', '555', '2022-12-03T17:22:28.814+00:00');
 
 -- Generating list of showtimes
 INSERT INTO showtime (id, movieid, showroom, showtime)
