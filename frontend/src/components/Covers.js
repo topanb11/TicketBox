2fa73f30adbc828108f54de4a0233097f4b34afe
topanb11 <topanbudiman11@gmail.com>
2022-12-04 01:24:14 -0700

    Fixed movie images

--- frontend/src/components/Covers.js
+++ frontend/src/components/Covers.js
@@ -1,12 +1,13 @@
 const covers = [
-		{id: "193d976c-f370-43b9-b7a0-cd51dfcb6dec", cover: "https://media1.fdncms.com/connectsavannah/imager/u/slideshow/7210229/black-panther-poster.jpg"},
-		{id: "9088b6bc-68ed-46a4-83a7-ce93e56137f7", cover: "https://flxt.tmsimg.com/assets/p8548776_p_v8_ad.jpg"},
-		{id: "46ad04dc-ae33-4350-bf3e-350524678fef", cover: "https://m.media-amazon.com/images/M/MV5BOTlhYTVkMDktYzIyNC00NzlkLTlmN2ItOGEyMWQ4OTA2NDdmXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"},
-		{id: "878886d8-4bc8-47a4-8e42-27fc96384670", cover: "https://image.tmdb.org/t/p/original/vybQQ7w7vGvF53IsGD0y0JSgIsA.jpg"},
-		{id: "5a66573f-e95a-4d4e-8855-fa03a42c5b84", cover: "https://m.media-amazon.com/images/M/MV5BNGYyNmI3M2YtNzYzZS00OTViLTkxYjAtZDIyZmE1Y2U1ZmQ2XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg"},
-		{id: "36354fbc-e055-4350-a445-dc0efc0feb88", cover: "https://lumiere-a.akamaihd.net/v1/images/p_toystory3_19639_3c584e1f.jpeg"},
-		{id: "aafee6b5-accf-4411-a2a1-05b0d6cc2eba", cover: "https://upload.wikimedia.org/wikipedia/en/5/5d/AKIRA_%281988_poster%29.jpg"},
-		{id: "d8b49b72-084b-46ad-8126-825f216b19b2", cover: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg"},
-		{id: "dbf9d01e-fb01-4df8-ae05-a1e91f759ce7", cover: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg"},
+		{id: "7db4984a-62ee-42a2-b263-841d83bc27dd", cover: "https://media1.fdncms.com/connectsavannah/imager/u/slideshow/7210229/black-panther-poster.jpg"},
+		{id: "b3138a7f-b78f-41d4-82c1-74699e8cb1ec", cover: "https://flxt.tmsimg.com/assets/p8548776_p_v8_ad.jpg"},
+		{id: "cca5ab6b-dfb2-4b63-bcc9-70d53e8c3ee1", cover: "https://m.media-amazon.com/images/M/MV5BOTlhYTVkMDktYzIyNC00NzlkLTlmN2ItOGEyMWQ4OTA2NDdmXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"},
+		{id: "3e3dfd12-0a4e-45b5-80c1-9b8da0c69174", cover: "https://image.tmdb.org/t/p/original/vybQQ7w7vGvF53IsGD0y0JSgIsA.jpg"},
+		{id: "b7736ee7-9002-48d2-9336-37d51b19cdad", cover: "https://m.media-amazon.com/images/M/MV5BNGYyNmI3M2YtNzYzZS00OTViLTkxYjAtZDIyZmE1Y2U1ZmQ2XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg"},
+		{id: "18f69d95-00c7-4995-b812-c8271b65cc06", cover: "https://lumiere-a.akamaihd.net/v1/images/p_toystory3_19639_3c584e1f.jpeg"},
+		{id: "bb06b752-8295-476d-af69-2ad2f6f1f0ff", cover: "https://upload.wikimedia.org/wikipedia/en/5/5d/AKIRA_%281988_poster%29.jpg"},
+		{id: "e6447cee-cb8e-4e5c-8d74-d2e7a22d39f9", cover: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg"},
+		{id: "576a300c-c9ab-410f-b576-139720dc84a8", cover: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg"},
+		{id: "79223ffa-7407-47e0-ab81-45bda1ba874d", cover: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_FMjpg_UX1000_.jpg"},
 	]
 export default covers;
