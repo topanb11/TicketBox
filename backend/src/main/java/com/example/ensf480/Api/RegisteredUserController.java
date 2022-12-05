c52f444908d3d033d876569d4e94649dd0a88990
topanb11 <topanbudiman11@gmail.com>
2022-12-05 12:17:03 -0700

    Added comments for controllers

--- backend/src/main/java/com/example/ensf480/Api/RegisteredUserController.java
+++ backend/src/main/java/com/example/ensf480/Api/RegisteredUserController.java
@@ -12,27 +12,33 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import com.example.ensf480.Model.RegisteredUser;
 import com.example.ensf480.Service.RegisteredUserService;
 
+// API routes for Registered user entity
 @RequestMapping("api/v1/user")
 @RestController
 @CrossOrigin
 public class RegisteredUserController {
+		// Instance of UserService to access methods
     private final RegisteredUserService userService;
 
+		// Dependency injection
     @Autowired
     public RegisteredUserController(RegisteredUserService userService) {
         this.userService = userService;
     }
 
+		// API route to instantiate a new RegisteredUser entity
     @PostMapping(path = "create")
     public RegisteredUser registerUser(@RequestBody RegisteredUser user) {
         return userService.registerUser(user);
     }
 
+		// API route to validate RegisteredUser info
     @PostMapping(path = "login")
     public RegisteredUser checkCredentials(@RequestBody Map<String, Object> userMap) {
         return userService.login(userMap);
     }
 
+		// API route to instantiate a renew a RegisteredUsers account
     @PostMapping(path = "reactivate")
     public RegisteredUser reactivate(@RequestBody Map<String, Object> userMap) {
         return userService.reactivate(userMap);
