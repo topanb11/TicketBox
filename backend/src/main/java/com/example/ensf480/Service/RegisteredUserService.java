2eaec29704cd7ab97c5a1eadfca0171efb1261ec
topanb11 <topanbudiman11@gmail.com>
2022-12-05 18:48:13 -0700

    Add comments to registereduserservice

--- backend/src/main/java/com/example/ensf480/Service/RegisteredUserService.java
+++ backend/src/main/java/com/example/ensf480/Service/RegisteredUserService.java
@@ -9,30 +9,52 @@ import org.springframework.stereotype.Service;
 import com.example.ensf480.Dao.RegisteredUserDao;
 import com.example.ensf480.Model.RegisteredUser;
 
+// Class for RegisteredUserSerfice
 @Service
 public class RegisteredUserService {
     private final RegisteredUserDao userDao;
 
+		// Dependency Injection
     @Autowired
     public RegisteredUserService(@Qualifier("Postgres") RegisteredUserDao userDao) {
         this.userDao = userDao;
     }
 
+		/**
+		 * Method to register user
+		 * @param person - RegisteredUser
+		 * @return RegisteredUser
+		 */
     public RegisteredUser registerUser(RegisteredUser person) {
         return userDao.insertPerson(person);
     }
 
+		/**
+		 * Method to log user in
+		 * @param userMap - User data 
+		 * @return RegisteredUser
+		 */
     public RegisteredUser login(Map<String, Object> userMap) {
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
         return userDao.login(email, password);
     }
 
+		/**
+		 * Method to reactivate RegisteredUser
+		 * @param userMap - User data
+		 * @return RegisteredUser
+		 */
     public RegisteredUser reactivate(Map<String, Object> userMap) {
         String id = (String) userMap.get("id");
         return userDao.reactivate(id);
     }
 
+		/**
+		 * Method to get user by email
+		 * @param email - User email
+		 * @return RegisteredUser
+		 */
     public RegisteredUser getUserByEmail(String email) {
         return userDao.getUserByEmail(email);
     }
